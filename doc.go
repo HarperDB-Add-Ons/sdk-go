005171c0bc0970aebee0ad1892931eb5c4d419b4
= <=>
2023-05-05 12:16:14 +0000

    Add test to catch timestamps from harperdb causing issues

--- doc.go
+++ doc.go
@@ -5,11 +5,10 @@ very easy to get up and running with HarperDB and your Go application.
 
 For more information see: https://docs.harperdb.io/
 
-Basics
+# Basics
 
 Instantiate a new client:
 
 	client := harperdb.NewClient("http://localhost:9925", "username", "password")
-
 */
 package harperdb
--- types_test.go
+++ types_test.go
@@ -1,13 +1,25 @@
 package harperdb
 
-func ExampleRecord() {
+import "testing"
 
-	type Player struct {
-		Record // embed the Record type in your custom types
+type Player struct {
+	Record        // embed the Record type in your custom types
+	Name   string `json:"name"`
+}
 
-		ID   int    `json:"id"`
-		Name string `json:"name"`
-		Age  int    `json:"age"`
+func ExampleRecord() Player {
+	return Player{
+		Name: "Jane",
+		Record: Record{
+			UpdatedTime: 123123.23324,
+		},
 	}
+}
 
+func TestRecordFloatTime(t *testing.T) {
+	str := ExampleRecord()
+
+	// Will panic if there's an issue.
+	str.UpdatedTime.ToTime()
+	str.CreatedTime.ToTime()
 }
